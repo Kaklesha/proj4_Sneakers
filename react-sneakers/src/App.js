bb8d8f4069b5400dc1472d853a5659381bfa5319
kira <k-korni1ov@yandex.ru>
2023-07-29 21:52:41 +0400

    feat: flex item

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between">
+        <div className="d-flex align-center">
+          <img src="/img/Logo.png" alt="Logo.png" />
+          <div>
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кросовок</p>
+          </div>
+        </div>
+
+        <ul className="headreRight d-flex justify-between">
+          <li className="mr-30">
+            <img src="/img/Group.svg" alt="Group.svg" />
+            <span>1205 rub</span>
+          </li>
+          <li>
+            <img src="/img/Union.svg" alt="Union.svg" />
+          </li>
+        </ul>
       </header>
+
+    
+        <div className="content p-40">
+          <h1 className="mb-40">Все кросовки</h1>  
+          
+          <div className="sneakers">
+                <div className="card">
+                  <img
+                    width={133}
+                    height={112}
+                    src="/img/sneakers/image 5.jpg"
+                    alt=""
+                  />
+                  <h5>Мужские Кроссовки Nike </h5>
+                  <div className="d-flex justify-between align-center">
+                    <div className="d-flex flex-column ">
+                      <span>Цена:</span>
+                      <b>12999руб</b>
+                    </div>
+                    <button className="button">
+                      <img
+                        width={11}
+                        height={11}
+                        src="/img/sneakers/Group 91.svg"
+                        alt="plus.svg"
+                      />
+                    </button>
+                  </div>
+                </div>
+                
+                <div className="card">
+          <img
+            width={133}
+            height={112}
+            src="/img/sneakers/image 5.jpg"
+            alt=""
+          />
+          <h5>Мужские Кроссовки Nike </h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column ">
+              <span>Цена:</span>
+              <b>12999руб</b>
+            </div>
+            <button className="button">
+              <img
+                width={11}
+                height={11}
+                src="/img/sneakers/Group 91.svg"
+                alt="plus.svg"
+              />
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            width={133}
+            height={112}
+            src="/img/sneakers/image 5.jpg"
+            alt=""
+          />
+          <h5>Мужские Кроссовки Nike </h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column ">
+              <span>Цена:</span>
+              <b>12999руб</b>
+            </div>
+            <button className="button">
+              <img
+                width={11}
+                height={11}
+                src="/img/sneakers/Group 91.svg"
+                alt="plus.svg"
+              />
+            </button>
+          </div>
+        </div>
+        <div className="card">
+          <img
+            width={133}
+            height={112}
+            src="/img/sneakers/image 5.jpg"
+            alt=""
+          />
+          <h5>Мужские Кроссовки Nike </h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column ">
+              <span>Цена:</span>
+              <b>12999руб</b>
+            </div>
+            <button className="button">
+              <img
+                width={11}
+                height={11}
+                src="/img/sneakers/Group 91.svg"
+                alt="plus.svg"
+              />
+            </button>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
